4f3a723b2608dab1e6ab88ed9bd846a2413e2c62
Matt Hunt <wmatthunt@gmail.com>
2020-11-12 16:54:07 -0500

    added store option which clears previous list

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var socket = require('socket.io')
 
 //App setup
 var app = express();
-var server = app.listen(4000, function(){console.log("listening to request on port 4000")})
+var server = app.listen(4000, function () { console.log("listening to request on port 4000") })
 
 //static files
 app.use(express.static('public'));
@@ -11,14 +11,18 @@ app.use(express.static('public'));
 //socket setup
 var io = socket(server);
 
-io.on('connection', function(socket){
+io.on('connection', function (socket) {
     console.log("made socket connection", socket.id)
 
-    socket.on('chat', function(data){
-    io.sockets.emit('chat', data)
+    socket.on('chat', function (data) {
+        io.sockets.emit('chat', data)
     });
 
-    socket.on('typing', function(data){
+    socket.on('start-shop', function (data) {
+        io.sockets.emit('start-shop', data)
+    });
+
+    socket.on('typing', function (data) {
         socket.broadcast.emit('typing', data)
     });
 });
