c0a9cee77bc399ab9488777c2927a407b86b03b3
Matt Hunt <wmatthunt@gmail.com>
2020-11-14 13:28:21 -0500

    stuff

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,7 @@
 import React, {useEffect, useState } from 'react';
 import UserInfo from "./pages/UserInfo";
-<<<<<<< HEAD
-import ShoppingList from "./pages/ShoppingList.js";
-=======
 import socket from "./utils/socket/socket";
 
->>>>>>> 7868c3cfeb64bf1f14d2dd6bc043af6b2eb910e8
 
 function App() {
   const [chatMessage, setChatMessage] = useState("");
@@ -20,14 +16,9 @@ function App() {
   }, [])
   return (
     <div className="App">
-<<<<<<< HEAD
-      {/* <UserInfo /> */}
-      <ShoppingList/>
-=======
       <UserInfo />
       {chatMessage}
       <button onClick={() => {socket.emit("chat", "socket works!")}}>emit</button>
->>>>>>> 7868c3cfeb64bf1f14d2dd6bc043af6b2eb910e8
     </div>
   );
 }
