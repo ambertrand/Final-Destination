32fffccda1e573fa1122e863994f98a34804d177
nkleinmann <nekleinmann@gmail.com>
2020-11-14 11:42:07 -0500

    trying to clean git log

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,5 @@
-<<<<<<< HEAD
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
 import UserInfo from "./pages/UserInfo";
 
 function App() {
@@ -8,13 +7,6 @@ function App() {
     <div className="App">
       <UserInfo />
     </div>
-=======
-import React from "react";
-
-function App() {
-  return (
-    <h1>App</h1>
->>>>>>> ecc081d5844ad89d52086e8c263cdbc8e5d8264c
   );
 }
 
