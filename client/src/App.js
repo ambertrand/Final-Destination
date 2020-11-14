1f5e3774f7e2608d8cbf1a5991cacfc5324ed572
nkleinmann <nekleinmann@gmail.com>
2020-11-14 13:11:39 -0500

    socket connection working

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,22 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import UserInfo from "./pages/UserInfo";
+import socket from "./utils/socket/socket";
+
 
 function App() {
+  useEffect( () => {
+    socket.on("chat", (data) => {
+      console.log(data);
+    });
+    return () => {
+      socket.off("chat");
+    }
+    // dependency array, put user object in array if we want the callback function to fire everytime userobject sees a change
+  }, [])
   return (
     <div className="App">
       <UserInfo />
+      <button onClick={() => {socket.emit("chat", "socket works!")}}>emit</button>
     </div>
   );
 }
