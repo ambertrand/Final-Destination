4034da6401218471488235b7c820ef0553d796e9
Matt Hunt <wmatthunt@gmail.com>
2020-11-16 11:00:17 -0500

    commenting out comps for now

--- client/src/pages/ShoppingList.js
+++ client/src/pages/ShoppingList.js
@@ -8,25 +8,38 @@ import "./shopStyle.css";
 function Shopping() {
     const [chatMessage, setChatMessage] = useState("");
     useEffect(() => {
-      socket.on("chat", (data) => {
-        setChatMessage(data);
-      });
-      return () => {
-        socket.off("chat");
-      }
-      // dependency array, put user object in array if we want the callback function to fire everytime userobject sees a change
+        socket.on("chat", (data) => {
+            setChatMessage(data);
+        });
+        return () => {
+            socket.off("chat");
+        }
+        // dependency array, put user object in array if we want the callback function to fire everytime userobject sees a change
     }, [])
     return (
         <div>
-        <p>Shopping list works!</p>
-        {chatMessage}
-        <label>Socket in shopping list</label>
-        <button onClick={() => { socket.emit("chat", "socket works!") }}>emit</button>
-        <div id="list-chat">
-           <StartList/>
-           <ChatWindow/>
-           <AddList/>
+            <p>Shopping list works!</p>
+            {chatMessage}
+            <label>Socket in shopping list</label>
+            {/* <button onClick={() => { socket.emit("chat", "socket works!") }}>emit</button> */}
+            {/* <div id="list-chat">
+                <StartList />
+                <ChatWindow />
+                <AddList />
+            </div> */}
+            <div id="list-chat">
+        <input id="store" type="text" placeholder="Store"></input>
+        <button id="shop">Start List</button>
+        <div id = "store-div"></div>
+        <div id="chat-window">
+            <div id="output"></div>
+            <div id="feedback"></div>
+            
         </div>
+        <input id="handle" type="text" placeholder="Handle"></input>
+        <input id="message" type="text" placeholder="message"></input>
+        <button id="send"onClick={() => { socket.emit("chat", "socket works!") }}>Send</button>
+    </div>
         </div>
     )
 }
