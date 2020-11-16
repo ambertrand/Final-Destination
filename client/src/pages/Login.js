f203e88018972b80982b0792e7ed5d8cdd777529
Jesse Farmer <drjfarmer@gmail.com>
2020-11-16 16:06:52 -0500

    Login

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,8 +1,21 @@
 import React, { useEffect, useState } from "react";
-
+import "../styles/Login.css";
 function Login() {
     return (
-        <p>Login works!</p>
+        <div class="jumbotron jumbotron-fluid">
+            <div class="container">
+                <h1 class="display-4">Welcome To While You're There!</h1>
+
+                < div class="info">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer sapien nunc, blandit eu aliquam ut, tincidunt eu ante. In hac habitasse platea dictumst. Maecenas vel interdum tortor. Cras suscipit nisi at turpis consequat pulvinar. Nulla massa dui, porttitor vel sodales quis, euismod ut nunc. Sed accumsan tempor eros. Donec fermentum porttitor ullamcorper. Morbi tempus placerat dui, at consectetur enim ultrices id.
+
+</p>
+                </div>
+                <div class="pic">
+                    <img src="https://i.pinimg.com/originals/d7/dd/94/d7dd9454070b491daa5dc6b38fb679f5.jpg" alt="Logo" width="200" height="200" />;
+                </div>
+            </div>
+        </div>
     )
 }
 
