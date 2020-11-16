05159d55c5e2d2d4dcbd5d4cd35e016b7ed54f6c
Matt Hunt <wmatthunt@gmail.com>
2020-11-16 10:15:41 -0500

    put socket in shopping

--- client/src/pages/ShoppingList.js
+++ client/src/pages/ShoppingList.js
@@ -1,8 +1,24 @@
 import React, { useEffect, useState } from "react";
+import socket from "../utils/socket/socket";
 
 function Shopping() {
+    const [chatMessage, setChatMessage] = useState("");
+    useEffect(() => {
+      socket.on("chat", (data) => {
+        setChatMessage(data);
+      });
+      return () => {
+        socket.off("chat");
+      }
+      // dependency array, put user object in array if we want the callback function to fire everytime userobject sees a change
+    }, [])
     return (
+        <div>
         <p>Shopping list works!</p>
+        {chatMessage}
+        <label>Socket in shopping list</label>
+        <button onClick={() => { socket.emit("chat", "socket works!") }}>emit</button>
+        </div>
     )
 }
 
