c7bdd59e5961222b169611529d7fb7193882b78b
Matt Hunt <wmatthunt@gmail.com>
2020-11-16 19:06:45 -0500

    stuff

--- client/src/pages/ShoppingList.js
+++ client/src/pages/ShoppingList.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import socket from "../utils/socket/socket";
 // import StartList from "../components/Chat/StartList";
-// import ChatWindow from "../components/Chat/ChatWindow";
+//import ChatWindow from "../components/Chat/ChatWindow";
 // import AddComment from "../components/Chat/AddComment";
 import "./shopStyle.css";
 
@@ -20,7 +20,8 @@ function Shopping() {
     }, [])
     return (
         <div>
-            <p>Shopping list works!</p>
+            <h1>Shopping List</h1>
+            {/* <p>Shopping list works!</p> */}
             {/* {chatMessage} */}
             <label>Socket in shopping list</label>
             {/* <button onClick={() => { socket.emit("chat", "socket works!") }}>emit</button> */}
@@ -30,8 +31,8 @@ function Shopping() {
                 <AddComment />
             </div> */}
             <div id="list-chat">
-                <input id="store" type="text" placeholder="Store"></input>
-                <button id="shop">Start List</button>
+                {/* <input id="store" type="text" placeholder="Store"></input>
+                <button id="shop">Start List</button> */}
                 <div id="store-div"></div>
                 <div id="chat-window">
                     <div id="output">
@@ -40,9 +41,9 @@ function Shopping() {
                     <div id="feedback"></div>
 
                 </div>
-                <input id="handle" type="text" placeholder="Handle"></input>
+                {/* <input id="handle" type="text" placeholder="Handle"></input> */}
                 <input id="message" type="text" placeholder="message"></input>
-                <button id="send" onClick={() => { socket.emit("chat", "User:" + message) }}>Send</button>
+                <button id="send" onClick={() => { socket.emit("chat", "User: " + message) }}>Send</button>
             </div>
         </div>
     )
