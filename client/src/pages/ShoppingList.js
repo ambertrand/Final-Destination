89cccf4f1cc54614f7f1f150ea3ffbcb0379b430
Matt Hunt <wmatthunt@gmail.com>
2020-11-16 20:23:07 -0500

    got rid of uneeded comps and chat works

--- client/src/pages/ShoppingList.js
+++ client/src/pages/ShoppingList.js
@@ -1,24 +1,26 @@
 import React, { useEffect, useState, useRef } from "react";
 import socket from "../utils/socket/socket";
-// import StartList from "../components/Chat/StartList";
-//import ChatWindow from "../components/Chat/ChatWindow";
-// import AddComment from "../components/Chat/AddComment";
 import "./shopStyle.css";
-
+var user = "User";
+var store = "Store"
+var storeMessage = user + " is going to " + store
 function Shopping() {
     const messageRef = useRef();
-    const typingRef = useRef();
-    // const [chatMessage, setChatMessage] = useState("");
     const [messages, setMessages] = useState([]);
-    const handleSendMessage = () => { socket.emit("chat", "User: " + messageRef.current.value) }
-    const handleTyping = () => {socket.emit("typing", "User: " + " is typing")}
+    const [typing, setTyping] = useState("")
+    const handleSendMessage = () => {
+        socket.emit("chat", user + ": " + messageRef.current.value)
+        messageRef.current.value = ""
+    }
+    const handleTyping = () => { socket.emit("typing", user + ": is typing") }
     useEffect(() => {
         socket.on("chat", (data) => {
             setMessages((prevMessages) => ([...prevMessages, data]));
-            console.log(data)
+            setTyping("")
         });
         socket.on("typing", (data) => {
-          console.log("is typing")
+            setTyping(data)
+            console.log(data)
         });
         return () => {
             socket.off("chat");
@@ -27,31 +29,18 @@ function Shopping() {
     }, [])
     return (
         <div>
-            <h1>Shopping List</h1>
-            {/* <p>Shopping list works!</p> */}
-            {/* {chatMessage} */}
-            {/* <label>Socket in shopping list</label> */}
-            {/* <button onClick={() => { socket.emit("chat", "socket works!") }}>emit</button> */}
-            {/* <div id="list-chat">
-                <StartList />
-                <ChatWindow />
-                <AddComment />
-            </div> */}
+            <h1>{storeMessage}</h1>
             <div id="list-chat">
-                {/* <input id="store" type="text" placeholder="Store"></input>
-                <button id="shop">Start List</button> */}
-                <div id="store-div"></div>
                 <div id="chat-window">
                     <div id="output">
                         {messages.map((message, i) => (
                             <p key={i}>{message}</p>
                         ))}
                     </div>
-                    <div id="feedback" ref={typingRef}></div>
+                    <div id="feedback">{typing}</div>
 
                 </div>
-                {/* <input id="handle" type="text" placeholder="Handle"></input> */}
-                <input id="message" type="text" placeholder="message" ref={messageRef} onInput={handleTyping}/>
+                <input id="message" autoComplete="off" type="text" placeholder="message" ref={messageRef} onChange={handleTyping} />
                 <button id="send" onClick={handleSendMessage}>Send</button>
             </div>
         </div>
