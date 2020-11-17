5e1fb0e5fc8271132714d02851b084bbac3001be
Matt Hunt <wmatthunt@gmail.com>
2020-11-16 21:20:55 -0500

    stuff

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import UserInfo from "./pages/UserInfo";
 import Shopping from "./pages/ShoppingList";
 import Login from "./pages/Login";
