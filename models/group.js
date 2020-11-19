34c2ede29875f62b7aa2eac6a08996b95b0b724e
Alex Bertrand <alex.m.bertrand@gmail.com>
2020-11-18 19:50:32 -0500

    created group model

---
+++ models/group.js
@@ -0,0 +1,18 @@
+const Sequelize = require("sequelize");
+
+module.exports = function(sequelize, DataTypes) {
+
+    const Group = sequelize.define("group", {
+        id: {
+            type: DataTypes.UUID,
+            defaultValue: Sequelize.UUIDV4,
+            allowNull: false,
+            unique: true,
+            primaryKey: true
+        },
+        group_name: {
+            type: DataTypes.STRING
+        }
+    });
+    return Group;
+}
