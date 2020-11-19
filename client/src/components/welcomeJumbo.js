58ed70bdfb4f39725f9eb2c3e29db26c7d4cbd79
Jesse Farmer <drjfarmer@gmail.com>
2020-11-18 20:15:45 -0500

    jesse

--- client/src/components/welcomeJumbo.js
+++ client/src/components/welcomeJumbo.js
@@ -10,7 +10,13 @@ function WelcomeJumbo() {
                 <Col xs="auto">
                     <h2>Welcome to While You're There</h2>
                 </Col>
+            </Row>
+            <Row className="justify-content-center">
+                <Col xs="auto">
 
+                    <img className="animate__animated animate__bounceInLeft" src="https://static.thenounproject.com/png/1700380-200.png" width="100px" alt="shopping cart" />
+                    {/* <img src="./assets/shoppingCart.png" width="100px" alt="shopping cart"/> */}
+                </Col>
             </Row>
         </Jumbotron>
     )
