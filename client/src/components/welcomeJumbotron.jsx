a6254f9b5c522308c81b973e91ac13922c46e537
Jesse Farmer <drjfarmer@gmail.com>
2020-11-18 21:18:40 -0500

    jesse

--- client/src/components/welcomeJumbotron.jsx
+++ client/src/components/welcomeJumbotron.jsx
@@ -2,10 +2,11 @@ import React from "react";
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Jumbotron from 'react-bootstrap/Jumbotron';
+import "./jumboStyle.css";
 
-function WelcomeJumbo() {
+function JumboSection() {
     return (
-        <Jumbotron fluid>
+        <Jumbotron fluid className="jumbo">
             <Row className="justify-content-center">
                 <Col xs="auto">
                     <h2>Welcome to While You're There</h2>
@@ -13,7 +14,6 @@ function WelcomeJumbo() {
             </Row>
             <Row className="justify-content-center">
                 <Col xs="auto">
-
                     <img className="animate__animated animate__bounceInLeft" src="https://static.thenounproject.com/png/1700380-200.png" width="100px" alt="shopping cart" />
                     {/* <img src="./assets/shoppingCart.png" width="100px" alt="shopping cart"/> */}
                 </Col>
@@ -22,4 +22,4 @@ function WelcomeJumbo() {
     )
 }
 
-export default WelcomeJumbo;
+export default JumboSection;
