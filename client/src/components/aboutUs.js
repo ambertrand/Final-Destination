ffc41f04430325594990a23d9db9d333240ab63d
Jesse Farmer <drjfarmer@gmail.com>
2020-11-18 19:15:20 -0500

    login changes

--- client/src/components/aboutUs.js
+++ client/src/components/aboutUs.js
@@ -10,10 +10,10 @@ function AboutUs() {
     return (
         <div>
 
-            <Row>
-                <Col sm></Col>
-                <Col sm>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer sapien nunc, blandit eu aliquam ut, tincidunt eu ante. In hac habitasse platea dictumst. Maecenas vel interdum tortor. Cras suscipit nisi at turpis consequat pulvinar. Nulla massa dui, porttitor vel sodales quis, euismod ut nunc. Sed accumsan tempor eros. Donec fermentum porttitor ullamcorper. Morbi tempus placerat dui, at consectetur enim ultrices id.</Col>
-                <Col sm></Col>
+            <Row className="justify-content-center mb-4">
+
+                <Col xs={4} >Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer sapien nunc, blandit eu aliquam ut, tincidunt eu ante. In hac habitasse platea dictumst. Maecenas vel interdum tortor. Cras suscipit nisi at turpis consequat pulvinar. Nulla massa dui, porttitor vel sodales quis, euismod ut nunc. Sed accumsan tempor eros. Donec fermentum porttitor ullamcorper. Morbi tempus placerat dui, at consectetur enim ultrices id.</Col>
+
             </Row>
 
 
