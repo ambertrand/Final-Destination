fc8251d3aebc6f7d64dd3813ef121080313bb927
Dwayne Risley <RisleyDwayne@gmail.com>
2020-11-21 11:24:26 -0500

    sketched out lib and routes

---
+++ routes/users.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const router = express.Router();
+const db = require("../models");
+
+router.get('/user', function(req, res, next){
+    const { _raw, _json, ...userProfile } = req.userProfile;
+
+    //get current user
+    db.userProfile.findOne({
+        where: {
+            auth0_id: userProfile.id
+        }
+    }).then((userProfile) => {
+        const hbsObject = {
+
+            user: {
+                id: userProfile.id,
+                email: userProfile.email,
+                picture: userProfile.picture,
+                firstName: userProfile.firstName,
+                lastName: userProfile.lastName,
+
+            }
+        }
+
+        res.render('user', hbsObject);
+        
+    }).catch(err => console.log(err));
+});
+
+
+module.exports = router;
