61afe2cd4e3141dc236f3021546d1e4822f36576
nkleinmann <nekleinmann@gmail.com>
2020-11-22 11:09:55 -0500

    cleaning git log

--- client/src/components/navigation/Logout.jsx
+++ client/src/components/navigation/Logout.jsx
@@ -22,29 +22,22 @@ const Logout = () => {
           {/* Set up Router */}
           <Row className="navigation navbar navbar-light bg-dark">
             <Col sm={12} className="navigation-sub">
-              <Link to="/" className="item navbar-brand text-white">Login</Link>
-              <Link to="/home" className="item navbar-brand text-white">Home</Link>
+              <Link to="/" className="item navbar-brand text-white">Home</Link>
               <Link to="/userinfo" className="item navbar-brand text-white">Profile</Link>
               <Link to="/shopping" className="item navbar-brand text-white">Shopping </Link>
-              <Link to="/about" className="item navbar-brand text-white">About/Contact Us </Link>
-
+              <Link to="/home" className="item navbar-brand text-white">About/Contact Us </Link>
               <Link to="/" className="item navbar-brand text-white" onClick={() => logout()}>
                 Log Out
                     {/* <Button onClick={() => logout()}>
                       Log Out
                     </Button> */}
               </Link>
-
             </Col>
           </Row>
-
           <Route exact path="/" component={Login} />
-          <Route exact path="/home" component={Home} />
           <Route path="/userinfo" component={UserInfo} />
           <Route exact path="/shopping" component={Shopping} />
-          <Route exact path="/about" component={About} />
-
-
+          <Route exact path="/home" component={Home} />
         </Router>
 
 
