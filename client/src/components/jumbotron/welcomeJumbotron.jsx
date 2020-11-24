91ac3daaceeabde83209eab063fa8429f8d5ae0a
Jesse Farmer <drjfarmer@gmail.com>
2020-11-23 21:31:28 -0500

    jesse

--- client/src/components/jumbotron/welcomeJumbotron.jsx
+++ client/src/components/jumbotron/welcomeJumbotron.jsx
@@ -1,25 +1,25 @@
-import React from "react";
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import Jumbotron from 'react-bootstrap/Jumbotron';
-import "./jumboStyle.css";
+// import React from "react";
+// import Row from 'react-bootstrap/Row';
+// import Col from 'react-bootstrap/Col';
+// import Jumbotron from 'react-bootstrap/Jumbotron';
+// import "./jumboStyle.css";
 
-function JumboSection() {
-    return (
-        <Jumbotron fluid className="jumbo jumbotron-fluid">
-            <Row className="justify-content-center">
-                <Col xs="auto">
-                    <h2>Welcome to While You're There</h2>
-                </Col>
-            </Row>
-            <Row className="justify-content-center">
-                <Col xs="auto">
-                    <img className="animate__animated animate__bounceInLeft" src="https://static.thenounproject.com/png/1700380-200.png" width="100px" alt="shopping cart" />
-                    {/* <img src="./assets/shoppingCart.png" width="100px" alt="shopping cart"/> */}
-                </Col>
-            </Row>
-        </Jumbotron>
-    )
-}
+// function JumboSection() {
+//     return (
+//         <Jumbotron fluid className="jumbo jumbotron-fluid">
+//             <Row className="justify-content-center">
+//                 <Col xs="auto">
+//                     <h2>Welcome to While You're There</h2>
+//                 </Col>
+//             </Row>
+//             <Row className="justify-content-center">
+//                 <Col xs="auto">
+//                     <img className="animate__animated animate__bounceInLeft" src="https://static.thenounproject.com/png/1700380-200.png" width="100px" alt="shopping cart" />
+//                     {/* <img src="./assets/shoppingCart.png" width="100px" alt="shopping cart"/> */}
+//                 </Col>
+//             </Row>
+//         </Jumbotron>
+//     )
+// }
 
-export default JumboSection;
+// export default JumboSection;
