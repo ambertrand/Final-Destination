2c52272c4e5173026fd08c262cba83a59760a8c6
Jesse Farmer <drjfarmer@gmail.com>
2020-11-26 09:45:46 -0500

    Carousel attemp

--- client/src/components/aboutComponents/aboutUs.jsx
+++ client/src/components/aboutComponents/aboutUs.jsx
@@ -3,6 +3,7 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Card from 'react-bootstrap/Card';
 
+
 function AboutUs() {
     return (
         <div>
@@ -14,13 +15,13 @@ function AboutUs() {
                     <Card className="bg-light">
                         <Card.Body className="text-center">
                             <p>
-                            We definitely have! That is why 
-                            we created While You're There. Now with COVID-19 it is more important then ever to remember shopping items 
-                            in one trip. This helps high risk individuals be able to request and receive all of their shopping needs 
-                            and minimizes the times the shopper is in crowded stores. 
-                            We're here to save you time and heartache when it comes to running errands!
+                                We definitely have! That is why
+                                we created While You're There. Now with COVID-19 it is more important then ever to remember shopping items
+                                in one trip. This helps high risk individuals be able to request and receive all of their shopping needs
+                                and minimizes the times the shopper is in crowded stores.
+                                We're here to save you time and heartache when it comes to running errands!
                             </p>
-                            </Card.Body>
+                        </Card.Body>
                     </Card>
 
                 </Col>
--- client/src/views/About.jsx
+++ client/src/views/About.jsx
@@ -26,6 +26,9 @@ function About() {
 
             </Row>
 
+
+
+
         </Container>
 
 
