425b6d3145719494a04151757dec847279829d7b
Alex Bertrand <alex.m.bertrand@gmail.com>
2020-12-02 13:06:05 -0500

    seperating out home button logic into components

---
+++ client/src/components/homeComponents/newGroup.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const newGroupButton = (event) => {
+    event.preventDefault();
+    // console.log("button clicked");
+}
+
+export default newGroupButton
---
+++ client/src/components/homeComponents/viewGroup.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const viewGroupButton = (event) => {
+    event.preventDefault();
+    // console.log("button clicked");
+}
+
+
+export default viewGroupButton;
