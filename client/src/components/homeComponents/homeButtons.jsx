a6be956c477261782e74dca1e7f29638cb5ebf2e
Alex Bertrand <alex.m.bertrand@gmail.com>
2020-12-01 20:38:35 -0500

    centering home icons

--- client/src/components/homeComponents/homeButtons.jsx
+++ client/src/components/homeComponents/homeButtons.jsx
@@ -1,35 +1,78 @@
-import React from "react";
+import React from 'react';
+// import axios from 'axios';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
-import { CardImg } from "react-bootstrap";
+import Image from "react-bootstrap/Image";
 //  Buttons
 import addgroup from '../../assets/addgroup.png';
 import viewgroups from '../../assets/viewgroups.png';
 import editinfo from '../../assets/editinfo.png';
 import stores from '../../assets/stores.png';
 
+
 function HomeButtons() {
+
+    const clickButtons = (event) => {
+        event.preventDefault();
+        // console.log("button clicked");
+    }
+
 
     return (
         <Container>
             <Row className="teams">
-                <Col sm="12" md="3" className="mb-3">
-                    <CardImg className="iconImg align-content-center" src={addgroup} />
-                    <Button type="submit" className="choice m-2" id="createTeam">Create Group</Button>
+                <Col sm={12} md={3}>
+                    <Row>
+                        <Col className="text-center">
+                            <Image className="iconImg" src={addgroup} />
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col>
+                            <Button type="submit" className="choice m-2" id="createTeam" onClick={clickButtons} >Create Group</Button>
+                        </Col>
+                    </Row>
                 </Col>
-                <Col sm="12" md="3" className="mb-3">
-                    <CardImg className="iconImg" src={viewgroups} />
-                    <Button type="submit" className="choice m-2" id="viewTeams">View Groups</Button>
+
+                <Col sm={12} md={3}>
+                    <Row>
+                        <Col className="text-center">
+                            <Image className="iconImg" src={viewgroups} />
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col>
+                            <Button type="submit" className="choice m-2" id="viewTeams">View Groups</Button>
+                        </Col>
+                    </Row>
                 </Col>
-                <Col sm="12" md="3" className="mb-3">
-                    <CardImg className="iconImg" src={editinfo} />
-                    <Button type="submit" className="choice m-2" id="updateUserProfile">Update User Profile</Button>
+
+                <Col sm={12} md={3}>
+                    <Row>
+                        <Col className="text-center">
+                            <Image className="iconImg" src={editinfo} />
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col>
+                            <Button type="submit" className="choice m-2" id="updateUserProfile">Update User Profile</Button>
+                        </Col>
+                    </Row>
                 </Col>
-                <Col sm="12" md="3" className="mb-3">
-                    <CardImg className="iconImg" src={stores} />
-                    <Button type="submit" className="choice m-2" id="viewGroupStores">View Group Stores</Button>
+
+                <Col sm={12} md={3}>
+                    <Row>
+                        <Col className="text-center">
+                            <Image className="iconImg" src={stores} />
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col>
+                            <Button type="submit" className="choice m-2" id="viewGroupStores">View Group Stores</Button>
+                        </Col>
+                    </Row>
                 </Col>
             </Row>
         </Container>
