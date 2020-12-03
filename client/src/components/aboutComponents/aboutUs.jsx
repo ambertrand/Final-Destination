5e7b6cac02f390033a18b878c784f207f58e7b6f
Jesse Farmer <drjfarmer@gmail.com>
2020-12-02 21:23:44 -0500

    aboutus

--- client/src/components/aboutComponents/aboutUs.jsx
+++ client/src/components/aboutComponents/aboutUs.jsx
@@ -7,12 +7,15 @@ import alex from '../../assets/Alex.png';
 import nk from '../../assets/NK.png';
 import matthew from '../../assets/Matthew.png';
 import jfarms from '../../assets/jfarms.png';
+import warren from '../../assets/warren.png';
+import Dwayne from '../../assets/Dwayne.png';
 import { First } from "react-bootstrap/esm/PageItem";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 
 function AboutUs() {
     return (
-        <div>
+        <div className="carouselPad">
 
             <Row className="justify-content-center mb-4">
 
@@ -21,7 +24,7 @@ function AboutUs() {
                         <Card style={{ width: '18rem' }}>
                             <Card.Img variant="top" src={alex} />
                             <Card.Body>
-                                <Card.Title>Card Title</Card.Title>
+                                <Card.Title>Alex Kleinmann</Card.Title>
                                 <Card.Text>
                                     Some quick example text to build on the card title and make up the bulk of
                                     the card's content.
@@ -33,7 +36,7 @@ function AboutUs() {
                         <Card style={{ width: '18rem' }}>
                             <Card.Img variant="top" src={nk} />
                             <Card.Body>
-                                <Card.Title>Card Title</Card.Title>
+                                <Card.Title>Nicole Kleinmann</Card.Title>
                                 <Card.Text>
                                     Some quick example text to build on the card title and make up the bulk of
                                     the card's content.
@@ -45,7 +48,13 @@ function AboutUs() {
                         <Card style={{ width: '18rem' }}>
                             <Card.Img style={{ width: 500, height: 'auto' }} variant="top" src={matthew} />
                             <Card.Body>
-                                <Card.Title>Card Title</Card.Title>
+                                <Card.Title>Matthew Bongiorno</Card.Title>
+
+                                <a href="https://www.google.com/">
+
+                                    <FontAwesomeIcon icon={['fab', 'FaLinkedin']} />
+
+                                </a>
                                 <Card.Text>
                                     Some quick example text to build on the card title and make up the bulk of
                                     the card's content.
@@ -58,7 +67,7 @@ function AboutUs() {
                         <Card style={{ width: '18rem' }}>
                             <Card.Img variant="top" src={jfarms} />
                             <Card.Body>
-                                <Card.Title>Card Title</Card.Title>
+                                <Card.Title>Jesse Farmer</Card.Title>
                                 <Card.Text>
                                     Some quick example text to build on the card title and make up the bulk of
                                     the card's content.
@@ -68,9 +77,9 @@ function AboutUs() {
                     </Carousel.Item>
                     <Carousel.Item>
                         <Card style={{ width: '18rem' }}>
-                            <Card.Img variant="top" src={jfarms} />
+                            <Card.Img variant="top" src={Dwayne} />
                             <Card.Body>
-                                <Card.Title>Card Title</Card.Title>
+                                <Card.Title>Dwayne Risley</Card.Title>
                                 <Card.Text>
                                     Some quick example text to build on the card title and make up the bulk of
                                     the card's content.
@@ -79,15 +88,16 @@ function AboutUs() {
                         </Card>
                     </Carousel.Item>
                     <Carousel.Item>
-                        <img
-                            className="d-block w-100"
-                            src=""
-                            alt="First slide"
-                        />
-                        <Carousel.Caption>
-                            <h3>First slide label</h3>
-                            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-                        </Carousel.Caption>
+                        <Card style={{ width: '18rem' }}>
+                            <Card.Img variant="top" src={warren} />
+                            <Card.Body>
+                                <Card.Title>Warren Hunt</Card.Title>
+                                <Card.Text>
+                                    Some quick example text to build on the card title and make up the bulk of
+                                    the card's content.
+                            </Card.Text>
+                            </Card.Body>
+                        </Card>
                     </Carousel.Item>
                 </Carousel>
 
