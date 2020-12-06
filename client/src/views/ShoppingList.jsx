fd3c5457d7644195e21182023293e75348a4f2af
nkleinmann <nekleinmann@gmail.com>
2020-12-05 20:41:00 -0500

    trying to get user image

--- client/src/views/ShoppingList.jsx
+++ client/src/views/ShoppingList.jsx
@@ -5,12 +5,11 @@ import { useAuth0 } from '@auth0/auth0-react';
 function Shopping({history}) {
     const { isAuthenticated } = useAuth0();
     React.useEffect(() => {
-        // console.log(isAuthenticated);
         if (!isAuthenticated) {
             history.push("/")
         }
     }, [isAuthenticated, history]);
-    
+
     return (
         <div>
             <Chat/>
