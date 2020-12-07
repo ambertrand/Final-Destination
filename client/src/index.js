1924a81d78b3143672a89ec4435c3a46d776776c
Dwayne Risley <RisleyDwayne@gmail.com>
2020-12-07 18:41:07 -0500

    changing to auth provider with history

--- client/src/index.js
+++ client/src/index.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import { Auth0Provider } from '@auth0/auth0-react';
+import Auth0ProviderWithHistory  from './auth/Auth0ProviderWithHistory';
 
 const domain = process.env.REACT_APP_AUTH0_DOMAIN
 const clientId = process.env.REACT_APP_AUTH0_CLIENTID
 
 ReactDOM.render(
-  <Auth0Provider
+  <Auth0ProviderWithHistory
     domain={domain}
     clientId={clientId}
     redirectUri={(window.location.origin) + "/auth-callback"}
   >
     <App />
-  </Auth0Provider>,
+  </Auth0ProviderWithHistory>,
   document.getElementById('root')
 );
