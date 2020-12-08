5186695b6593a77b31454055d615e99ce07dfc87
Alex Bertrand <alex.m.bertrand@gmail.com>
2020-12-08 12:31:41 -0500

    Testing socket for deployment

--- client/src/utils/socket/socket.js
+++ client/src/utils/socket/socket.js
@@ -5,7 +5,7 @@
 import io from 'socket.io-client';
 let socket;
 export const initiateSocket = (room) => {
-  socket = io('http://localhost:3000');
+  socket = io(process.env.PORT || 'http://localhost:3000');
   console.log(`Connecting socket...`);
   if (socket && room) socket.emit('join', room);
 }
