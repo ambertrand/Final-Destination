b307f48bf863df2efedd7efdab172f08394bc292
Dwayne Risley <RisleyDwayne@gmail.com>
2020-12-08 13:42:21 -0500

    bad import

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from "react-router-dom";
-import  App  from './App';
 import Auth0ProviderWithHistory  from './auth/Auth0ProviderWithHistory';
 import App from './App';
 import { Auth0Provider } from '@auth0/auth0-react';
