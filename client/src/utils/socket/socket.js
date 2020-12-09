3bd0aa20866e5c7322a7043732f2c501fc48c318
Alex Bertrand <alex.m.bertrand@gmail.com>
2020-12-09 11:51:29 -0500

    Removing commented out code

--- client/src/utils/socket/socket.js
+++ client/src/utils/socket/socket.js
@@ -1,7 +1,3 @@
-// import {io} from "socket.io-client";
-// const socket = io.connect(process.env.PORT || "http://localhost:3001");
-//export default socket;
-
 import axios from 'axios';
 import io from 'socket.io-client';
 let socket;
